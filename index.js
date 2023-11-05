cad67844cce9d7727c0aadd7111d0e74367693d8
aliAldwahy <amrostark@gmail.com>
2023-11-05 14:54:59 +0300

    port

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ app.use("/api/user", userRouter);
 
  
 
-// app.listen(process.env.PORT || 3000, function(){
-//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-//   });
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
-app.listen(PORT, () => {
-    console.log(`server is running at Port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`server is running at Port ${PORT}`);
+// });
