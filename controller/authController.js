559c02151255c38ac4214204dd905633b0828a94
alialdwahy <aligowi358@gmail.com>
2023-11-13 13:46:15 +0300

    forgot password

--- controller/authController.js
+++ controller/authController.js
@@ -8,6 +8,7 @@ const asyncHandler = require('express-async-handler');
 const validateMongoDbId = require("../utils/validateMongodbid");
 const { generateRefreshToken } = require("../config/refreshtoken");
 const jwt = require("jsonwebtoken");
+const sendEmail = require("../controller/emailController");
 const crypto = require("crypto-js");
 
 
@@ -162,47 +163,47 @@ res.json({
     }
  });
 
-//  const forgotPasswordToken = asyncHandler(async(req, res) => {
-//       const { email } = req.body;
-//       const user = await User.findOne({email});
-//       if (!user) throw new Error("User not found with this email");
-//       try {
-//         const token = await user.createPasswordResetToken();
-//         await user.save();
-//         const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. 
-//         <a href = 'http://localhost:5000/api/user/reset-password/${token}'> Click Here</>`;
-//         const data = {
-//             to: email,
-//             text: "Hey User",
-//             subject: "Forgot Password Link",
-//             htm: resetURL,
-//         };
-//         sendEmail(data);
-//         res.json(token);
+ const forgotPasswordToken = asyncHandler(async(req, res) => {
+      const { email } = req.body;
+      const user = await User.findOne({email});
+      if (!user) throw new Error("User not found with this email");
+      try {
+        const token = await user.createPasswordResetToken();
+        await user.save();
+        const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. 
+        <a href = 'http://localhost:5000/api/user/reset-password/${token}'> Click Here</>`;
+        const data = {
+            to: email,
+            text: "Hey User",
+            subject: "Forgot Password Link",
+            htm: resetURL,
+        };
+        sendEmail(data);
+        res.json(token);
 
-//       } catch (error) {
-//         throw new Error(error);
-//       }
-//  });
+      } catch (error) {
+        throw new Error(error);
+      }
+ });
 
 
-//  const resetPassword = asyncHandler( async(req, res) => {
-//     const { password } = req.body;
-//     const { token } = req.params;
-//     const hashedToken = crypto.createHash("sha256").update(token).digest("hes");
-//     const user = await User.findOne({
-//         passwordResetToken: hashedToken,
-//         passwordResetExpires: { $gt: Date.now() },
+ const resetPassword = asyncHandler( async(req, res) => {
+    const { password } = req.body;
+    const { token } = req.params;
+    const hashedToken = crypto.createHash("sha256").update(token).digest("hes");
+    const user = await User.findOne({
+        passwordResetToken: hashedToken,
+        passwordResetExpires: { $gt: Date.now() },
        
-//     });
-//     console.log(Date.now());
-//     if (!user) throw new Error("Token Expired, Please try again later");
-//     user.password = password;
-//     user.passwordResetToken = undefined;
-//     user.passwordResetExpires= undefined;
-//     await user.save();
-//     res.json(user);
-//  });
+    });
+    console.log(Date.now());
+    if (!user) throw new Error("Token Expired, Please try again later");
+    user.password = password;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires= undefined;
+    await user.save();
+    res.json(user);
+ });
 
  
 
@@ -212,7 +213,7 @@ module.exports = {
        handlerRefreshToken,
        logout,
        updatePassword,
-    //    forgotPasswordToken,
-    //    resetPassword,
+       forgotPasswordToken,
+       resetPassword,
        loginAdmin,
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
-const {createUser,loginUser,handlerRefreshToken,logout,updatePassword,loginAdmin} = require("../controller/authController");
-const { authMiddleware} = require("../middlewares/authmiddleware");
+const {createUser,loginUser,handlerRefreshToken,logout,updatePassword,loginAdmin,forgotPasswordToken,resetPassword} = require("../controller/authController");
+const { authMiddleware, isAdmin} = require("../middlewares/authmiddleware");
 
 const router = express.Router();
 
@@ -9,10 +9,12 @@ const router = express.Router();
 // Auth Router
 router.post("/register", createUser);
 router.post("/login", loginUser);
-router.post("/login-admin", loginAdmin);
+router.post("/login-admin", loginAdmin,isAdmin);
 router.put("/update-password",  authMiddleware,updatePassword);
 router.get("/refreshToken", handlerRefreshToken );
 router.get("/logout", logout );
+router.post("/forgot-password", forgotPasswordToken);
+router.put("/reset-password/:token", resetPassword);
 
 
 
