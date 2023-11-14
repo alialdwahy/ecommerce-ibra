eeef8605e067409be924b988e95f99d734de721b
alialdwahy <aligowi358@gmail.com>
2023-11-14 13:40:00 +0300

    tesr

--- controller/authController.js
+++ controller/authController.js
@@ -190,8 +190,7 @@ res.json({
  const resetPassword = asyncHandler( async(req, res) => {
     const { password } = req.body;
     const { token } = req.params;
-    const hashedToken = crypto.SHA256(token).toString(crypto.enc.Base64);
-
+    const hashedToken = crypto.createHash("sha256").update(token).digest("hes");
     const user = await User.findOne({
         passwordResetToken: hashedToken,
         passwordResetExpires: { $gt: Date.now() },
